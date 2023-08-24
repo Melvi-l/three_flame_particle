1e54dd6775e38d8e0f6c0bcc12ff9ff67d648ed3
melvil <melvil.miglio@cy-tech.fr>
2023-08-24 23:24:53 +0200

    impl smoke

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GUI } from 'lil-gui'
 import flammeVertex from "./shaders/flamme/vertex.glsl"
 import flammeFragment from "./shaders/flamme/fragment.glsl"
+import smokeVertex from "./shaders/smoke/vertex.glsl"
+import smokeFragment from "./shaders/smoke/fragment.glsl"
 
 let camera, scene, renderer, controls, debug; // ThreeJS globals
-let flammeParticle;
+let flammeParticle, smokeParticle;
 let sizes;
 
 init()
@@ -28,7 +30,7 @@ function init() {
 
   // Camera
   camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-  camera.position.set(2, 2, 2)
+  camera.position.set(15, 2, 15)
   scene.add(camera)
 
   // Controls
@@ -47,6 +49,7 @@ function init() {
 
   addTestLight()
   addFlamme()
+  addSmoke()
 }
 
 
@@ -58,7 +61,7 @@ function addTestLight() {
   scene.add(ambientLight, directionalLight)
 }
 
-function generateFlammeParticle(count, size, radius, height, speedFactor, innerColor, outerColor) {
+function generateFlammeParticle(position, count, size, radius, height, speedFactor, innerColor, outerColor) {
   if (flammeParticle) {
     flammeParticle.geometry.dispose()
     flammeParticle.material.dispose()
@@ -71,9 +74,9 @@ function generateFlammeParticle(count, size, radius, height, speedFactor, innerC
     // Polar coordinate
     const polarRadius = Math.random()
     const polarAngle = Math.random() * 2 * Math.PI
-    flammeParticlePosition[i * 3 + 0] = polarRadius * radius * Math.cos(polarAngle)
-    flammeParticlePosition[i * 3 + 1] = (Math.random() - 0.5) * height
-    flammeParticlePosition[i * 3 + 2] = polarRadius * radius * Math.sin(polarAngle)
+    flammeParticlePosition[i * 3 + 0] = position.x + polarRadius * radius * Math.cos(polarAngle)
+    flammeParticlePosition[i * 3 + 1] = position.y + Math.random() * height
+    flammeParticlePosition[i * 3 + 2] = position.z + polarRadius * radius * Math.sin(polarAngle)
 
     flammeParticleScale[i] = Math.random()
   }
@@ -88,6 +91,7 @@ function generateFlammeParticle(count, size, radius, height, speedFactor, innerC
     vertexShader: flammeVertex,
     fragmentShader: flammeFragment,
     uniforms: {
+      u_position: {value: position},
       u_radius: { value: radius },
       u_height: { value: height },
       u_size: { value: size },
@@ -105,23 +109,99 @@ function generateFlammeParticle(count, size, radius, height, speedFactor, innerC
 
 function addFlamme() {
   const debugObject = {
-    count: 1000,
-    size: 100,
-    flammeRadius: 0.5,
+    position: new THREE.Vector3(),
+    count: 1100,
+    size: 130,
+    flammeRadius: 0.6,
     flammeHeight: 2.5,
     speedFactor: 0.5,
-    innerColor: new THREE.Color(0xf69d9d),
-    outerColor: new THREE.Color(0xe0e316),
+    innerColor: new THREE.Color(0xFFF2A0),
+    outerColor: new THREE.Color(0xD60808),
   }
-  const regen = () => generateFlammeParticle(debugObject.count, debugObject.size, debugObject.flammeRadius, debugObject.flammeHeight, debugObject.speedFactor, debugObject.innerColor, debugObject.outerColor)
+  const regen = () => generateFlammeParticle(debugObject.position, debugObject.count, debugObject.size, debugObject.flammeRadius, debugObject.flammeHeight, debugObject.speedFactor, debugObject.innerColor, debugObject.outerColor)
   regen()
-  debug.add(debugObject, "count").min(100).max(3000).step(10).name("Particle count").onFinishChange(regen)
-  debug.add(debugObject, "size").min(0).max(500).step(1).name("Particle size").onFinishChange(regen)
-  debug.add(debugObject, "flammeRadius").min(0).max(4).step(0.01).onFinishChange(regen)
-  debug.add(debugObject, "flammeHeight").min(0).max(8).step(0.01).onFinishChange(regen)
-  debug.add(debugObject, "speedFactor").min(0).max(3).step(0.01).onFinishChange(regen)
-  debug.addColor(debugObject, "innerColor").onFinishChange(regen)
-  debug.addColor(debugObject, "outerColor").onFinishChange(regen)
+  const folder = debug.addFolder("flamme")
+  folder.add(debugObject, "count").min(100).max(3000).step(10).name("Particle count").onFinishChange(regen)
+  folder.add(debugObject, "size").min(0).max(500).step(1).name("Particle size").onFinishChange(regen)
+  folder.add(debugObject, "flammeRadius").min(0).max(4).step(0.01).onFinishChange(regen)
+  folder.add(debugObject, "flammeHeight").min(0).max(8).step(0.01).onFinishChange(regen)
+  folder.add(debugObject, "speedFactor").min(0).max(3).step(0.01).onFinishChange(regen)
+  folder.addColor(debugObject, "innerColor").onFinishChange(regen)
+  folder.addColor(debugObject, "outerColor").onFinishChange(regen)
+}
+
+function generateSmokeParticle(position, count, size, radius, height, speedFactor, windForce, windWaveFactor, windWavePeriod) {
+  if (smokeParticle) {
+    smokeParticle.geometry.dispose()
+    smokeParticle.material.dispose()
+    scene.remove(smokeParticle)
+  }
+  const smokeParticleGeometry = new THREE.BufferGeometry()
+  const smokeParticlePosition = new Float32Array(count * 3)
+  const smokeParticleScale = new Float32Array(count)
+  for (let i = 0; i < count; ++i) {
+    // Polar coordinate
+    const polarRadius = Math.random()
+    const polarAngle = Math.random() * 2 * Math.PI
+    smokeParticlePosition[i * 3 + 0] = position.x + polarRadius * radius * Math.cos(polarAngle)
+    smokeParticlePosition[i * 3 + 1] = position.y + Math.random() * height
+    smokeParticlePosition[i * 3 + 2] = position.z + polarRadius * radius * Math.sin(polarAngle)
+
+    smokeParticleScale[i] = Math.random()
+  }
+  smokeParticleGeometry.setAttribute("position", new THREE.Float32BufferAttribute(smokeParticlePosition, 3))
+  smokeParticleGeometry.setAttribute("a_scale", new THREE.Float32BufferAttribute(smokeParticleScale, 1))
+
+  // Material
+  const smokeParticleMaterial = new THREE.ShaderMaterial({
+    transparent: true,
+    depthWrite: false,
+    blending: THREE.NormalBlending,
+    vertexShader: smokeVertex,
+    fragmentShader: smokeFragment,
+    uniforms: {
+      u_position: {value: position},
+      u_radius: { value: radius },
+      u_height: { value: height },
+      u_size: { value: size },
+      u_time: { value: 0 },
+      u_speedFactor: { value: speedFactor },
+      u_windForce: {value: windForce },
+      u_windWaveFactor: {value: windWaveFactor },
+      u_windWavePeriod: {value: windWavePeriod },
+    },
+  })
+
+  // Point
+  smokeParticle = new THREE.Points(smokeParticleGeometry, smokeParticleMaterial)
+  scene.add(smokeParticle)
+}
+
+function addSmoke() {
+  const debugObject = {
+    position: new THREE.Vector3(0,1.5,0),
+    count: 1100,
+    size: 130,
+    smokeRadius: 0.6,
+    smokeHeight: 5,
+    speedFactor: 0.5,
+    windForce: new THREE.Vector3(4,0,0),
+    windWaveFactor: 5,
+    windWavePeriod: 5
+  }
+  const regen = () => generateSmokeParticle(debugObject.position, debugObject.count, debugObject.size, debugObject.smokeRadius, debugObject.smokeHeight, debugObject.speedFactor, debugObject.windForce, debugObject.windWaveFactor, debugObject.windWavePeriod)
+  regen()
+  const folder = debug.addFolder("smoke")
+  folder.add(debugObject, "count").min(100).max(3000).step(10).name("Particle count").onFinishChange(regen)
+  folder.add(debugObject, "size").min(0).max(500).step(1).name("Particle size").onFinishChange(regen)
+  folder.add(debugObject, "smokeRadius").min(0).max(4).step(0.01).onFinishChange(regen)
+  folder.add(debugObject, "smokeHeight").min(0).max(8).step(0.01).onFinishChange(regen)
+  folder.add(debugObject, "speedFactor").min(0).max(3).step(0.01).onFinishChange(regen)
+  folder.add(debugObject.windForce, "x").min(-10).max(10).step(0.01).name("windX").onFinishChange(regen)
+  folder.add(debugObject.windForce, "y").min(-10).max(10).step(0.01).name("windY").onFinishChange(regen)
+  folder.add(debugObject.windForce, "z").min(-10).max(10).step(0.01).name("windZ").onFinishChange(regen)
+  folder.add(debugObject, "windWaveFactor").min(0).max(20).step(0.01).onFinishChange(regen)
+  folder.add(debugObject, "windWavePeriod").min(0).max(20).step(0.01).onFinishChange(regen)
 }
 
 // Animate
@@ -138,6 +218,9 @@ function animation() {
   // Update particle
   if (flammeParticle) {
     flammeParticle.material.uniforms.u_time.value = elapsedTimeSecond
+  }
+  if (smokeParticle) {
+    smokeParticle.material.uniforms.u_time.value = elapsedTimeSecond
   }
   // Render
   renderer.render(scene, camera)
--- shaders/smoke/vertex.glsl
+++ shaders/smoke/vertex.glsl
@@ -4,12 +4,16 @@ uniform float u_time;
 uniform float u_radius;
 uniform float u_height;
 uniform float u_speedFactor;
+uniform vec3 u_windForce;
+uniform float u_windWaveFactor;
+uniform float u_windWavePeriod;
 
 attribute float a_scale;
 
 varying float v_yPositionNormalized;
 varying float v_distanceToCenterNormalized;
 varying float v_scale;
+varying vec2 v_uv;
 
 void main() {
     float P_MASS = (a_scale + 0.5)/1.5;
@@ -20,20 +24,28 @@ void main() {
     absolutePosition.y += u_time * u_speedFactor / P_MASS;
 
     vec4 loopedPosition = absolutePosition;
-    float distanceToCenter =  distance(loopedPosition.xz, u_position.xz);
+    float distanceToCenter = distance(loopedPosition.xz, u_position.xz);
     float yRelative = absolutePosition.y - u_position.y;
     float yLoopedRelative = fract(yRelative/u_height)*u_height + pow(distanceToCenter,2.);
     loopedPosition.y = u_position.y + yLoopedRelative;
     v_yPositionNormalized = yLoopedRelative / u_height;
 
+    loopedPosition.xz *= pow(1.+a_scale*v_yPositionNormalized, 2.) -.1;
+
+    float factorAttenuation = .1;
+    float periodAttenuation = .1;
+    float pseudoRandomVariation = (sin(loopedPosition.y*0.05 + u_time*u_windWavePeriod*periodAttenuation)+.8) * u_windWaveFactor * factorAttenuation;
+    loopedPosition.xyz += u_windForce * pow(v_yPositionNormalized, 2.) * pseudoRandomVariation;
 
     vec4 viewPosition = viewMatrix * loopedPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectionPosition; 
-    gl_PointSize = u_size*a_scale*(1.2 - v_yPositionNormalized);
+    gl_PointSize = u_size*a_scale*(.5+v_yPositionNormalized);
     gl_PointSize *= (1. / -viewPosition.z);
 
     v_distanceToCenterNormalized = distanceToCenter/u_radius;
     v_scale = a_scale;
+    v_uv = uv;
+
 }
